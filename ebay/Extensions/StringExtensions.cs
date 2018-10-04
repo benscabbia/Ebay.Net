d711390c97646dd8cb82164b2750a6f4ea16ffe7
Benjamin Scabbia <benscabbia@gmail.com>
2018-10-04 21:32:04 +0100

    Added base64 string extensions

---
+++ ebay/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+namespace EbayNet.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Base64Encode(this string @this)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(@this);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(this string @this)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(@this);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
