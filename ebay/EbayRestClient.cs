47e0bf9e0c139e8063baa41284a43a964b8e0d29
Benjamin Scabbia <benscabbia@gmail.com>
2018-10-13 21:29:49 +0100

    Added ebayRestClient which will help pass token to the request

---
+++ ebay/EbayRestClient.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using EbayNet.Authentication;
+using EbayNet.BrowseAPIs;
+namespace EbayNet
+{
+    public sealed class EbayRestClient
+    {
+        private OAuth2Authenticator _oAuth2Authenticator;
+        public EbayRestClient(OAuth2Authenticator oAuth2Authenticator)
+        {
+           _oAuth2Authenticator = oAuth2Authenticator; 
+        }
+        public async Task<T> Request<T>(Func<string, Task<T>> request) 
+        {
+            var token = await _oAuth2Authenticator.GetTokenAsync();
+            var response = await request(token.AccessToken).ConfigureAwait(false);
+            return response;
+        }
+    }
+}
