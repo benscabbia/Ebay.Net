4dd2a6ac1a03ea17eb718b1a3312b3ec0f00a6f8
Benjamin Scabbia <benscabbia@gmail.com>
2018-10-27 20:40:00 +0100

    Updated unit tests

--- ebay.tests/OAuth2AuthenticatorTests.cs
+++ ebay.tests/OAuth2AuthenticatorTests.cs
@@ -15,8 +15,8 @@ namespace ebay.tests
         [InlineData("", "", "Og==")]
         [InlineData("a", "a", "YTph")]
         [InlineData("clientId", "clientSecret", "Y2xpZW50SWQ6Y2xpZW50U2VjcmV0")]
-        [InlineData("my-awesome-app-11ab1cd02-11z12345", 
-                    "ABC-1hg3jk1999ab-b123-456c-a12b-12aza", 
+        [InlineData("my-awesome-app-11ab1cd02-11z12345",
+                    "ABC-1hg3jk1999ab-b123-456c-a12b-12aza",
                     "bXktYXdlc29tZS1hcHAtMTFhYjFjZDAyLTExejEyMzQ1OkFCQy0xaGczamsxOTk5YWItYjEyMy00NTZjLWExMmItMTJhemE=")]
         public void OAuth2Authenticator_ShouldCorrectly_SetBase64Property(string clientId, string clientSecret, string expected)
         {
@@ -31,7 +31,7 @@ namespace ebay.tests
             var authenticator = new OAuth2Authenticator("clientId", "clientSecret");
             using(var httpTest = new HttpTest())
             {
-                var token = await authenticator.Authenticate(); 
+                var token = await authenticator.Authenticate();
                 httpTest
                     .ShouldHaveCalled("https://api.ebay.com/identity/v1/oauth2/token/")
                     .WithContentType("application/x-www-form-urlencoded")
@@ -41,14 +41,14 @@ namespace ebay.tests
                     .Times(1);
             }
         }
-        
+
         [Fact]
         public async Task OAuth2Authenticator_ShouldUse_SanboxUrl(){
-            var authenticator = new OAuth2Authenticator("clientId", "clientSecret", EbayNet.Environment.Sandbox);
-
+            var authenticator = new OAuth2Authenticator("clientId", "clientSecret");
+			authenticator.UrlService.Environment = EbayNet.Environment.Sandbox;
             using(var httpTest = new HttpTest())
             {
-                var token = await authenticator.Authenticate(); 
+                var token = await authenticator.Authenticate();
                 httpTest
                     .ShouldHaveCalled("https://api.sandbox.ebay.com/identity/v1/oauth2/token/")
                     .WithContentType("application/x-www-form-urlencoded")
--- ebay/BrowseAPIs/BrowseAPI.cs
+++ ebay/BrowseAPIs/BrowseAPI.cs
@@ -3,9 +3,9 @@ using EbayNet.Authentication;
 /*
 https://developer.ebay.com/api-docs/buy/browse/static/overview.html
 
-Search for and retrieve eBay items and use filters and parameters to create 
-customized item sets. It also provides the ability for eBay members to add 
-and remove items and change the quantity of an item in their eBay shopping 
+Search for and retrieve eBay items and use filters and parameters to create
+customized item sets. It also provides the ability for eBay members to add
+and remove items and change the quantity of an item in their eBay shopping
 cart as well as view the contents of their eBay cart.
 
 */
