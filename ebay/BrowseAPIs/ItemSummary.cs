56dd8cc42ddeb3e6389d4acebe08b2b65ffabeff
Benjamin Scabbia <benscabbia@gmail.com>
2018-11-17 15:36:02 +0000

    Item summary search API implemented

--- ebay/BrowseAPIs/ItemSummary.cs
+++ ebay/BrowseAPIs/ItemSummary.cs
@@ -1,5 +1,7 @@
-using System;
 using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using BrowseAPIs.Models;
 
 /*
     https://developer.ebay.com/api-docs/buy/browse/resources/methods
@@ -10,15 +12,27 @@ namespace EbayNet.BrowseAPIs
 {
     public sealed class ItemSummary
     {
+        private string _itemSummaryUrl = "buy/browse/v1/item_summary/search";
         private readonly EbayRestClient _ebayRestClient;
         public ItemSummary(EbayRestClient ebayRestClient)
         {
             _ebayRestClient = ebayRestClient;
         }
 
-        public Task Search()
+        public async Task<ItemSummaryCollectionModel> Search(string search, int limit = 50)
         {
-            throw new NotImplementedException();
+            var item = await _ebayRestClient.Request<ItemSummaryCollectionModel>(
+               _itemSummaryUrl
+                .AppendPathSegment($"?q={search}&limit={limit}", fullyEncode: true)
+                .WithHeaders(
+                    new
+                    {
+                        Content_Type = "application/json",
+                        X_EBAY_C_ENDUSERCTX = "contextualLocation=country=<2_character_country_code>,zip=<zip_code>,affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>"
+                    }, replaceUnderscoreWithHyphen: true)
+            );
+
+            return item;
         }
     }
 }
