a56cc03627f58d3a3afd8f103e43f0e8a11c0fe4
Benjamin Scabbia <benscabbia@gmail.com>
2018-12-08 16:27:21 +0000

    Added documentation to EbayClient to make it easy to find format

--- ebay/EbayClient.cs
+++ ebay/EbayClient.cs
@@ -9,11 +9,20 @@ namespace EbayNet
         public Environment Environment { get; private set; }
         public BrowseAPI BrowseAPI { get; private set; }
 
+        /// <summary>
+        /// EbayClient will take care and encode your clientID and clientSecret
+        /// </summary>
+        /// <param name="clientId">App ID (Client ID)</param>
+        /// <param name="clientSecret">Cert ID (Client Secret)</param>
         public EbayClient(string clientId, string clientSecret, Environment environment = Environment.Production)
         {
             Setup(new OAuth2Authenticator(clientId, clientSecret), environment);
         }
 
+        /// <summary>
+        /// Format specification: https://developer.ebay.com/api-docs/static/oauth-base64-credentials.html
+        /// </summary>
+        /// <param name="base64EncodedOAuthCredentials"> Base64 encoded "clientId:clientSecret" </param>
         public EbayClient(string base64EncodedOAuthCredentials, Environment environment = Environment.Production)
         {
             Setup(new OAuth2Authenticator(base64EncodedOAuthCredentials), environment);
